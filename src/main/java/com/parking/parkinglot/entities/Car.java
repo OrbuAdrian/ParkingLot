b26e974d304f0a4026ef33fbf52028262a4d8eb5
Adrian Orbu <adrianconstantin.orbu@ulbsibiu.ro>
2024-12-06 12:54:48 +0200

    "Added Car and User entities"

---
+++ src/main/java/com/parking/parkinglot/entities/Car.java
@@ -0,0 +1,56 @@
+package com.parking.parkinglot.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "car")
+public class Car {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "licensePlate")
+    private String licensePlate;
+
+    @Column(name = "parkingSpot")
+    private String parkingSpot;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public String getParkingSpot() {
+        return parkingSpot;
+    }
+
+    public void setParkingSpot(String parkingSpot) {
+        this.parkingSpot = parkingSpot;
+    }
+}
---
+++ src/main/java/com/parking/parkinglot/entities/User.java
@@ -0,0 +1,36 @@
+package com.parking.parkinglot.entities;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "id", nullable = false)
+  private Long id;
+
+  @OneToMany(mappedBy = "owner", orphanRemoval = true)
+  private List<Car> cars = new ArrayList<>();
+
+  public List<Car> getCars() {
+    return cars;
+  }
+
+  public void setCars(List<Car> cars) {
+    this.cars = cars;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Long getId() {
+    return id;
+  }
+}
